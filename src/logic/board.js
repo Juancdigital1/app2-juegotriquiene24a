d149087f11d7802b904d65b7a84a11531d36af32
unknown <jcan2412digital1@gmail.com>
2024-01-13 11:55:21 -0500

    primer commit

---
+++ src/logic/board.js
@@ -0,0 +1,25 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    // revisamos todas las combinacioes ganadoras 
+    // para ver si X u O gano 
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] 
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador 
+    return null
+  }
+
+  export const checkEndGame = (newBoard) => {
+    //revisamos si hay un empate
+    // si no hay mas espacios vacios
+    //en el tablero
+    return newBoard.every((square) => square != null) 
+  }
